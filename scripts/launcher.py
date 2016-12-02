ca672d18e43511ddb78ad494891f99a1efa19bd1
root <root@tensor-0.bigdata.research.intel-research.net>
2016-12-02 18:17:38 -0500

    correction to script

--- scripts/launcher.py
+++ scripts/launcher.py
@@ -9,8 +9,8 @@ parser = argparse.ArgumentParser()
 parser.add_argument("--name",help="Specify basename for vms (dont use '_' )")
 parser.add_argument("--num_workers", help="Specify number of workers",type=int)
 parser.add_argument("--num_parameter_servers", help="Specify number of parameter servers",type=int)
-parser.add_argument("--async",help="Specify if the training should be done synchronously or not")
-parser.add_argument("--num_sync",help="If async=True, how many workers should be synchronously updated",type=int)
+parser.add_argument("--sync",help="Specify if the training should be done synchronously or not")
+parser.add_argument("--num_sync",help="If sync=True, how many workers should be synchronously updated",type=int)
 parser.add_argument("--train_steps",help="Specify number of training steps",type=int)
 parser.add_argument("--approx_step",help="Step to start approximations",type=int)
 parser.add_argument("--approx_interval",help="Approximate every x steps",type=int)
@@ -22,7 +22,7 @@ args = parser.parse_args()
 name = args.name
 num_workers = args.num_workers
 num_ps = args.num_parameter_servers
-async = args.async
+sync = args.sync
 num_sync = args.num_sync
 train_steps = args.train_steps
 approx_step = args.approx_step
@@ -73,11 +73,11 @@ for i in range(num_workers):
 # first launching the parameter servers
 for i in range(num_ps):
         # tensorflow python args
-        if async == 'False':
+        if sync == 'False':
                 comm_ps = './tensorflow/models/image/cifar10_new/cifar10_replica_test1.py --ps_hosts=' \
         + ps_str + ' --worker_hosts=' + workers_str + ' --job_name="ps" --task_index=' + str(i) + \
-        ' --num_gpus=0 --train_steps=' + str(train_steps) + ' --sync_replicas=True' + ' --approx_step=' + str(approx_step) + ' --approx_interval=' + str(approx_interval) + ' --layers_to_train=' + str(layers_to_train)
-        if async == 'True':
+        ' --num_gpus=0 --train_steps=' + str(train_steps) + ' --sync_replicas=False' + ' --approx_step=' + str(approx_step) + ' --approx_interval=' + str(approx_interval) + ' --layers_to_train=' + str(layers_to_train)
+        if sync == 'True':
                 comm_ps = './tensorflow/models/image/cifar10_new/cifar10_replica.py --ps_hosts=' \
         + ps_str + ' --worker_hosts=' + workers_str + ' --job_name="ps" --task_index=' + str(i) + \
         ' --num_gpus=0 --train_steps=' + str(train_steps) + ' --sync_replicas=True --replicas_to_aggregate=' + str(num_sync)
@@ -101,14 +101,14 @@ for i in range(num_ps):
 # next launching the workers
 for i in range(num_workers):
         # tensorflow python args
-        if async == 'False':
+        if sync == 'False':
                 comm_worker = './tensorflow/models/image/cifar10_new/cifar10_replica_test1.py --ps_hosts=' \
         + ps_str + ' --worker_hosts=' + workers_str + ' --job_name="worker" --task_index=' + str(i) + \
-        ' --num_gpus=0 --train_steps=' + str(train_steps) + ' --sync_replicas=True' + ' --approx_step=' + str(approx_step) + ' --approx_interval=' + str(approx_interval) + ' --layers_to_train=' + str(layers_to_train)
-        if async == 'True':
+        ' --num_gpus=0 --train_steps=' + str(train_steps) + ' --sync_replicas=False' + ' --approx_step=' + str(approx_step) + ' --approx_interval=' + str(approx_interval) + ' --layers_to_train=' + str(layers_to_train)
+        if sync == 'True':
                 comm_worker = './tensorflow/models/image/cifar10_new/cifar10_replica.py --ps_hosts=' \
         + ps_str + ' --worker_hosts=' + workers_str + ' --job_name="worker" --task_index=' + str(i) + \
-        ' --num_gpus=0 --train_steps=' + str(train_steps) + ' --sync_replicas=False'
+        ' --num_gpus=0 --train_steps=' + str(train_steps) + ' --sync_replicas=True'
 
         #commands to launch on each vm
         comm0 = 'export PATH=\"$PATH:$HOME/bin\";'
